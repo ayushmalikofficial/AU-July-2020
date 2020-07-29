76c21d6c4c448340cf99e3636dca97eb9ba1de69
Ayush Malik <30635893+ayushmalikofficial@users.noreply.github.com>
2020-07-29 19:39:30 +0530

    Added more comments

--- Advanced Java-Afternoon/Question_2/Main.java
+++ Advanced Java-Afternoon/Question_2/Main.java
@@ -1,40 +1,41 @@
 package com.company;
 
-import org.dom4j.*;
-import java.sql.*;
-import java.io.File;
-import java.util.*;
-import org.dom4j.io.SAXReader;
 
+import java.io.File;
+import java.sql.*;
+import java.util.*;
+// For XML parsing
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 class Student
 {
-	//Connection Object
-	Connection con;
+    //Connection Object
+    Connection con;
 
+    //Constructor
     Student()
     {
         try{
-			//JDBC Connector has been used
+            //JDBC Connector has been used
             Class.forName("com.mysql.cj.jdbc.Driver");
-			//Make necessary changes as per your system's schema, port, username and password
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","amalik");
-			
-        }catch(Exception e)
-        {
+            //Make necessary changes as per your system's schema, port, username and password
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","amalik");
+        }
+        catch(Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-	//Function for adding a new row in Student Table
-	
-    public void addRow(int RollNo, String FirstName, String MiddleName, String LastName,String Gender, float Marks)
+    //Function for adding a new row in Student Table
+    public void AddRow(int RollNo, String FirstName, String MiddleName, String LastName,String Gender, float Marks)
     {
         try {
-
             PreparedStatement stmt = con.prepareStatement("insert into student values (?,?,?,?,?)");
             stmt.setInt(1, RollNo);
-            stmt.setString(2, FirstName+" "+MiddleName);
+            stmt.setString(2, FirstName+ " "+ MiddleName);
             stmt.setString(3, LastName);
             stmt.setString(4, Gender);
             stmt.setFloat(5, Marks);
@@ -43,27 +44,27 @@ class Student
             else
                 System.out.println("Insertion Failed");
 
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-	//Function for printing all rows of the Student Table
-	
-    public void printTableData()
+    //Function for printing all rows of the Student Table
+    public void ViewStudentTable()
     {
         try {
-            Statement stmt = con.createStatement();
-            ResultSet datatset =  stmt.executeQuery("select * from student");
-            while (datatset.next()) {
-                System.out.print(datatset.getInt("id")+"\t");
-                System.out.print(datatset.getString("Name")+"\t");
-                System.out.print(datatset.getString("Surname")+"\t");
-                System.out.print(datatset.getString("Gender")+"\t");
-                System.out.println(datatset.getFloat("Marks")+"\t");
+            Statement s = con.createStatement();
+            ResultSet DataSet =  s.executeQuery("select * from student");
+            while (DataSet.next()) {
+                System.out.print(DataSet.getInt("id")+"\t");
+                System.out.print(DataSet.getString("Name")+"\t");
+                System.out.print(DataSet.getString("Surname")+"\t");
+                System.out.print(DataSet.getString("Gender")+"\t");
+                System.out.println(DataSet.getFloat("Marks")+"\t");
             }
-
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
@@ -72,9 +73,9 @@ class Student
     {
         try{
             con.close();
-        }catch(Exception er)
-        {
-            System.out.println(er.getMessage());
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -83,25 +84,20 @@ class Student
 
 public class Main {
 
-    public static void main(String[] args)  {
 
+    private static void ParseXML(Student obj) throws DocumentException
+    {
 
-        // New Database Object
-		Student obj =  new Student();
-        
-		String FirstName=null,MiddleName=null,LastName=null,Gender=null;
+        String FirstName=null,MiddleName=null,LastName=null,Gender=null;
         int RollNo=0;
         float Marks=0;
-        try{
-			
-		// Extracting Data from XML File	
+        //==========================XML PARSING==========================
+
         // Make necessary changes to the path
-		
-		//==========================XML PARSING==========================
-		File xmlfile = new File("A:\\input.txt");
-        SAXReader saxReader = new SAXReader();
-        Document document = saxReader.read(xmlfile);
-        List<Node> nodes = document.selectNodes("/class/student");
+        File XMLFile = new File("A:\\input.txt");
+        SAXReader Sax = new SAXReader();
+        Document Doc = Sax.read(XMLFile);
+        List<Node> nodes = Doc.selectNodes("/class/student");
 
         for (Node node : nodes) {
             RollNo = Integer.parseInt(node.valueOf("@rollno"));
@@ -111,28 +107,35 @@ public class Main {
             Gender = node.selectSingleNode("gender").getText();
             Marks = Float.parseFloat(node.selectSingleNode("marks").getText());
         }
-		
-		//==================================================================
-		
-		
+        //==================================================================
+
+        // Adding data from XML file to Student Table
+        obj.AddRow(RollNo,FirstName,MiddleName,LastName,Gender,Marks);
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        // New Database Object
+        Student obj =  new Student();
+        try{
+
             System.out.println("Student Table before insertion of record from the given XML file\n");
-            obj.printTableData();
-			// Adding data from XML file to Student Table
-            obj.addRow(RollNo,FirstName,MiddleName,LastName,Gender,Marks);
-            //System.out.println(Marks);
+            obj.ViewStudentTable();
+
+            // Parsing input.txt and adding its content to Student Table
+            ParseXML(obj);
+
             System.out.println("Student Table after insertion of the record from the given XML file\n");
-            obj.printTableData();
-			
-			
-            }
+            obj.ViewStudentTable();
+        }
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        finally
-        {
+        finally {
             obj.close();
         }
-
 
     }
 }
